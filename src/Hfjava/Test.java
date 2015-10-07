060f18c34c682d0dd9ff26acba5f5a1219c96423
Vlad <viper2d@inbox.lv>
2015-10-07 22:01:58 +0300

    Initial commit

---
+++ src/Hfjava/Test.java
@@ -0,0 +1,27 @@
+package Hfjava;
+
+/**
+ * Created by Vlad on 07.10.2015.
+ */
+public class Test {
+    public static void main(String[] args) {
+        int x = 3;
+
+        while (x > 0){
+            if (x > 2){
+                System.out.print("a");
+            }
+            x = x - 1;
+            System.out.print("-");
+
+            if (x == 2){
+                System.out.print("b c");
+            }
+
+            if (x == 1){
+                System.out.print("d");
+                x = x - 1;
+            }
+        }
+    }
+}
